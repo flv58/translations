db62bc814de07bae7647a247e579dd8e553b4beb
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-03-17 11:57:55 +0200

    Weblate sucks

--- fix-weblate-languages.sh
+++ fix-weblate-languages.sh
@@ -7,19 +7,5 @@
 # Create a weblate.json file for importing all of the language components into Weblate
 #
 
-# Create the output
-rm -rf `pwd`/release/_weblate
-mkdir -p `pwd`/release/_weblate
-
-for d in */
-do
-    # Ignore some internal directories
-    if [[ "$d" == "build/" ]] || [[ "$d" == "_pages/" ]] || [[ "$d" == "release/" ]]
-    then
-        continue
-    fi
-
-    # All translation components reuse the repo checked out by the Akeeba Subs translation project, component_backend translation component for efficiency reasons
-    php ../buildfiles/tools/fix-weblate-language-files.php -r `pwd` -d $d
-done;
+php ../buildfiles/tools/fix-weblate-language-files.php -r `pwd`
 
